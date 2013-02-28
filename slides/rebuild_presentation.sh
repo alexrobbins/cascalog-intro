d9383a6150071d9879c9606341655078fad2f66d
Alex Robbins <alexander.j.robbins@gmail.com>
2013-02-28 15:26:40 -0600

    Fix generation to work from cold start.

--- slides/rebuild_presentation.sh
+++ slides/rebuild_presentation.sh
@@ -1,9 +1,13 @@
+printf "%b" "Rebuilding..."
+mdpress presentation.md
+cp -r images/ presentation
+printf "%b" "Done\n"
 printf "%b" "Watching presentation.md\n"
 while :
 do
+  inotifywait -e modify -q -q presentation.md
+  printf "%b" "File modified. "
   printf "%b" "Rebuilding..."
   mdpress presentation.md
   printf "%b" "Done\n"
-  inotifywait -e modify -q -q presentation.md
-  printf "%b" "File modified. "
 done
