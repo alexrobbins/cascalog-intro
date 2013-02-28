92badfa96873fba40985bfeed3bf89e1c92883a0
Alex Robbins <alexander.j.robbins@gmail.com>
2013-02-28 13:05:44 -0600

    Better messages, plus always rebuild once.

--- slides/rebuild_presentation.sh
+++ slides/rebuild_presentation.sh
@@ -1,8 +1,9 @@
 printf "%b" "Watching presentation.md\n"
 while :
 do
-  inotifywait -e modify -q -q presentation.md
-  printf "%b" "File modified. Rebuilding..."
+  printf "%b" "Rebuilding..."
   mdpress presentation.md
   printf "%b" "Done\n"
+  inotifywait -e modify -q -q presentation.md
+  printf "%b" "File modified. "
 done
